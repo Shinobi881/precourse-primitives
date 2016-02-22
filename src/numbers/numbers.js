d5dcb2500af974b8a5a347f5bbaffd393d205c3c
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-02-22 15:59:23 -0800

    (feat) Add numbers and booleans to repo

---
+++ src/numbers/numbers.js
@@ -0,0 +1,66 @@
+/*! strings v0.0.0 - MIT license */
+'use strict';
+
+// THIS FUNCTION IS AN EXAMPLE, DO NOT TOUCH
+var stringExample = function () {
+   // return the string 'this is just an example'
+   return 'this is just an example';
+}
+
+// YOUR CODE BELOW, VARIABLES ARE NOT ALLOWED FOR ANY PROBLEMS
+// THE POINT HERE IS TO IMPROVE YOUR UNDERSTANDING OF STRINGS AS A RAW DATA TYPE
+var stringIndex = function () {
+  // return the 3 letter of the string below
+  'the dream of Californication'
+  return 'the dream of Californication'[2];
+
+}
+
+var stringMaker = function () {
+  // bridge and return the two strings below using string concatenation
+ return 'California, rest in peace...' + ' Simutaneous release...';
+  
+}
+
+var stringSpecialChar = function () {
+  // fix an return the string below
+    // 'can't stop, won't stop';
+  return 'can\'t stop, won\'t stop'
+}
+
+var stringMultiLine = function () {
+  // Format and return the string so it will print on 3 lines
+  // This must all be done in one set of quotes
+  'string on line 1, string on line 2, string on line 3';
+  return 'string on line 1,\n string on line 2,\n string on line 3';
+  
+}
+
+var stringEquality = function () {
+  // return, whether or not the string below is of a string data type. 
+
+  // Example: return [special operator] 'string from below';
+  '35234235^%@^%@^%$@^$%@^#';
+  
+  return typeof '35234235^%@^%@^%$@^$%@^#';
+}
+
+var stringCount = function () {
+  // Return the number of characters in the string below
+  'You can blame it on rain.';
+  return 'You can blame it on rain.'.length;
+}
+
+
+var stringHelper1 = function () {
+  // Use a method on the string below to return the word 'wounded'
+  'Take time with a wounded hand';
+  return 'Take time with a wounded hand'.substring(17, 24);
+}
+
+var stringHelper2 = function () {
+  // Use a method to convert and return the string below into all uppercase letters
+  'Where ya going with tomorrow?';
+
+  return 'Where ya going with tomorrow?'.toUpperCase();
+}
